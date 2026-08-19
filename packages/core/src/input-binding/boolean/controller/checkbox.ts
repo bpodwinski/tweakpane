cdfb61c8197c4441c6312a3eb523500bf8f86a1c
Benoit Podwinski <git@benoitpodwinski.com>
2026-08-19 16:56:32 +0200

    Refactor CheckboxController: remove unused label mouse down event handler and bind

--- packages/core/src/input-binding/boolean/controller/checkbox.ts
+++ packages/core/src/input-binding/boolean/controller/checkbox.ts
@@ -24,7 +24,6 @@ export class CheckboxController
 
 	constructor(doc: Document, config: Config) {
 		this.onInputChange_ = this.onInputChange_.bind(this);
-		this.onLabelMouseDown_ = this.onLabelMouseDown_.bind(this);
 
 		this.value = config.value;
 		this.viewProps = config.viewProps;
@@ -34,10 +33,6 @@ export class CheckboxController
 			viewProps: this.viewProps,
 		});
 		this.view.inputElement.addEventListener('change', this.onInputChange_);
-		this.view.labelElement.addEventListener(
-			'mousedown',
-			this.onLabelMouseDown_,
-		);
 	}
 
 	private onInputChange_(ev: Event): void {
@@ -45,11 +40,5 @@ export class CheckboxController
 		this.value.rawValue = inputElem.checked;
 		ev.preventDefault();
 		ev.stopPropagation();
-	}
-
-	private onLabelMouseDown_(ev: MouseEvent): void {
-		// Prevent unwanted text selection
-		// https://github.com/cocopon/tweakpane/issues/545
-		ev.preventDefault();
 	}
 }
